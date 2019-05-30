ecee659e150f3a7dec23b7ca30feda9391ac52ff
diana_achim <diana_achim1998@yahoo.com>
2019-05-30 21:12:50 +0300

    daca esti curajoooos

---
+++ ProjectManagement/ProjectManagement/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ProjectManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TaskList = new System.Windows.Forms.DataGridView();
+            this.DeveloperList = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TaskList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DeveloperList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TaskList
+            // 
+            this.TaskList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TaskList.Location = new System.Drawing.Point(35, 35);
+            this.TaskList.Name = "TaskList";
+            this.TaskList.RowTemplate.Height = 24;
+            this.TaskList.Size = new System.Drawing.Size(316, 194);
+            this.TaskList.TabIndex = 0;
+            // 
+            // DeveloperList
+            // 
+            this.DeveloperList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DeveloperList.Location = new System.Drawing.Point(35, 263);
+            this.DeveloperList.Name = "DeveloperList";
+            this.DeveloperList.RowTemplate.Height = 24;
+            this.DeveloperList.Size = new System.Drawing.Size(316, 171);
+            this.DeveloperList.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(435, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 446);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DeveloperList);
+            this.Controls.Add(this.TaskList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TaskList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DeveloperList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TaskList;
+        private System.Windows.Forms.DataGridView DeveloperList;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProjectManagement/ProjectManagement/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjectManagement
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter daTask, daDeveloper;
+        DataSet ds;
+        SqlConnection dbConn;
+        SqlCommandBuilder cb;
+        BindingSource bsTask, bsDeveloper;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommandBuilder builder = new SqlCommandBuilder(daTask);
+            daTask.Update(ds.Tables["Task"]);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dbConn = new SqlConnection(@"Data Source = DESKTOP-5EB5T3U\SQLEXPRESS; Initial Catalog = ProjectManagement; Integrated Security = true");
+            ds = new DataSet();
+
+            daTask = new SqlDataAdapter("SELECT * FROM Task", dbConn);
+            daDeveloper = new SqlDataAdapter("SELECT * FROM Developer", dbConn);
+            cb = new SqlCommandBuilder(daTask);
+
+            DataRelation dr = new DataRelation("FK__Task__did__5441852A", ds.Tables["Developer"].Columns["did"], ds.Tables["Task"].Columns["did"]);
+
+            ds.Relations.Add(dr);
+
+            bsDeveloper = new BindingSource();
+            bsDeveloper.DataSource = ds;
+            bsDeveloper.DataMember = "Developer";
+
+            bsTask = new BindingSource();
+            bsTask.DataSource = bsDeveloper;
+            bsTask.DataMember = "FK__Task__did__5441852A";
+
+            this.DeveloperList.DataSource = bsDeveloper;
+            this.TaskList.DataSource = bsTask;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
